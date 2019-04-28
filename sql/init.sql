50afcc62634cca7b2996f6858c14225f5ac3a317
dychi <tnychor06@gmail.com>
2019-04-28 13:31:47 +0900

    hostを指定

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS mysite CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS djangouser IDENTIFIED WITH mysql_native_password BY 'djangopass';
-GRANT ALL ON mysite.* TO djangouser;
+CREATE USER IF NOT EXISTS djangouser@'%' IDENTIFIED WITH mysql_native_password BY 'djangopass';
+GRANT ALL ON mysite.* TO djangouser@'%';
 
 FLUSH PRIVILEGES;
